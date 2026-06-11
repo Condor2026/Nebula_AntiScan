c1c1a6b51731c15eb586ecb3c1b880c78d23169a
Condor2026 <268162817+Condor2026@users.noreply.github.com>
2026-06-12 00:50:14 +0200

    Update Nebula_AntiScan2.py

--- Nebula_AntiScan2.py
+++ Nebula_AntiScan2.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# SPDX-License-Identifier: GPL-3.0-or-later
+# SPDX-FileCopyrightText: 2026 Condor2026 / SpectrumSecurity
+#
+# KELTIC KRAKEN - Ireland Crime Intelligence Platform
+# Copyright (C) 2026 Condor2026
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
 """
 
 NEBULA ANTISCAN v2.0 – Detector de escaneos agresivos en tiempo real
